522b9e2a8e2f6e46bbe1db240519fabfe22cbff2
Matteo Poggi <matteo.poggi.fi@gmail.com>
2025-03-12 09:59:24 +0100

    Add patch to select branch in downloader.

--- downloader.sh
+++ downloader.sh
@@ -4,6 +4,6 @@ VAR3D_BRANCH=Multivariate
 HOME_3dvar=$PWD
 
 cd $HOME_3dvar
-git clone git@github.com:inogs/3dVarBio.git 3DVar
+git clone -b fix-local git@github.com:exactlab/3dVarBio.git 3DVar
 cd 3DVar
 git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
