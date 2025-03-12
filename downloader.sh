8c6172a7f7aac841bc71a14584cdf641654b89e8
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2025-03-12 11:02:26 +0100

    removing downloader
    deprecated by submodules

--- downloader.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-VAR3D_BRANCH=Multivariate
-HOME_3dvar=$PWD
-
-cd $HOME_3dvar
-git clone -b fix-local git@github.com:exactlab/3dVarBio.git 3DVar
-cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
